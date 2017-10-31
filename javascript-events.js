e587bbd80732ce9b8b36d6188db5d20cce09b3d1
Spjut-Audrey <31967527+Spjut-Audrey@users.noreply.github.com>
2017-10-30 23:53:21 -0400

    Update javascript-events.js

--- javascript-events.js
+++ javascript-events.js
@@ -33,3 +33,91 @@ function dateFunc() {
 
     document.getElementById("currentdate").innerHTML = n;
 }
+
+function numFunc(){
+    console.log("numFunc called");
+    var num_one = parseFloat(document.getElementById("numOne").value);
+    var num_two = parseFloat(document.getElementById("numTwo").value);
+
+    var s = num_one + num_two;
+    alert(s);
+//    return false;
+}
+
+function dragStart(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+}
+
+function dragging(event) {
+    document.getElementById("drop").innerHTML = " Element is dragging it's feet.";
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+    document.getElementById("drop").innerHTML = " You dropped it like it's hot!"
+}
+
+function copyFunc() {
+    document.getElementById("copyText").innerHTML = "Copy that, rogue leader";
+}
+
+function pasteFunc() {
+    document.getElementById("pasteText").innerHTML = "Stop pasting time! D:"
+}
+
+function printFunc() {
+    document.getElementById("printBox").innerHTML = "Printing in progress."
+}
+
+function mediaFunc() {
+    alert("Video is playing.");
+}
+
+var a = document.getElementById("aniDiv");
+
+function aniFunc() {
+a.style.WebkitAnimation = "name 10s 2";
+console.log(3);
+}
+
+a.addEventListener("webkitAnimationStart", startFunc);
+a.addEventListener("webkitAnimationIteration", repeatFunc);
+a.addEventListener("webkitAnimationEnd", endFunc);
+
+a.addEventListener("animationstart", startFunc);
+a.addEventListener("animationiteration", repeatFunc);
+a.addEventListener("animationend", endFunc);
+
+function startFunc() {
+     this.innerHTML = "animationstart event occurred";
+    this.style.backgroundColor = "blue";
+    console.log(2);
+}
+
+function repeatFunc() {
+    this.innerHTML = "animationiteration event occurred";
+    this.style.backgroundColor = "green";
+}
+
+function endFunc() {
+    this.innerHTML = "animationend event occurred";
+    this.style.backgroundColor = "purple";
+}
+
+document.getElementById("tranEvent").addEventListener("webkitTransitionEnd", tranFunc);
+document.getElementById("tranEvent").addEventListener("transitionend", tranFunc);
+
+function tranFunc() {
+    this.innerHTML = "transitionend event occurred";
+    this.style.backgroundColor = "red";
+}
+
+var obj = document.getElementById("touchBox").innerHTML;
+
+obj.addEventListener("touchmove", )
